f80e356177e40c8a4b118897763db2d496efdc9c
Gronify <thegronify@gmail.com>
2024-06-19 16:56:51 +0300

    frontent add weights and score

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,24 +6,56 @@ import { useState } from "react";
 
 export default function Home() {
   const [url, setUrl] = useState('')
-  const [data, setData] = useState({
-    firstContentfulPaint: 0,
-    largestContentfulPaint: 0,
-    firstMeaningfulPaint: 0,
-    speedIndex: 0,
-    totalBlockingTime: 0,
-    maxPotentialFid: 0,
-    cumulativeLayoutShift: 0,
-    serverResponseTime: 0,
-    timeToInteractive: 0,
+  const [weights, setWeights] = useState({
+    firstContentfulPaint: 0.55,
+    largestContentfulPaint: 0.55,
+    firstMeaningfulPaint: 0.55,
+    speedIndex: 0.55,
+    totalBlockingTime: 0.55,
+    maxPotentialFid: 0.55,
+    cumulativeLayoutShift: 0.55,
+    serverResponseTime: 0.55,
+    timeToInteractive: 0.55,
     metrics: 0
+  })
+  const [data, setData] = useState({
+    analyzeScore: 0,
+    measurement: {
+      firstContentfulPaint: 0,
+      largestContentfulPaint: 0,
+      firstMeaningfulPaint: 0,
+      speedIndex: 0,
+      totalBlockingTime: 0,
+      maxPotentialFid: 0,
+      cumulativeLayoutShift: 0,
+      serverResponseTime: 0,
+      timeToInteractive: 0,
+      metrics: 0
+    }
   })
   const handleChange = (prop: any) => (event: any) => {
     setUrl(event.target.value);
   };
-  function send(url: string) {
-    axios.get("http://localhost:5000/measurement", {
-      params: { url: url },
+
+  const handleRangeChange = (prop: any) => (event: any) => {
+    // dispatch(humanUpdateAction({ ...human, [prop]: event.target.value }))
+    setWeights({ ...weights, [prop]: event.target.value });
+  };
+  function send(url: string, weights: any) {
+    const weightsForRequest = {
+      fcp: weights.firstContentfulPaint,
+      lcp: weights.largestContentfulPaint,
+      fmp: weights.firstMeaningfulPaint,
+      si: weights.speedIndex,
+      tbt: weights.totalBlockingTime,
+      mpf: weights.maxPotentialFid,
+      cls: weights.cumulativeLayoutShift,
+      srt: weights.serverResponseTime,
+      tti: weights.timeToInteractive,
+      m: weights.metrics
+    }
+    axios.get("http://localhost:5000/analyzer", {
+      params: { url: url, ...weightsForRequest },
     }
     ).then((response: any) => {
       setData(response.data)
@@ -35,18 +67,17 @@ export default function Home() {
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
 
         <div className="max-w-xl mx-auto">
+          <label className="block text-gray-700 font-bold mb-2">Enter URL:</label>
+          <div className="flex justify-center align-center gap-2 mb-4">
 
-          <div className="flex justify-center gap-2">
 
-            <div className="mb-4">
-              <label className="block text-gray-700 font-bold mb-2">Enter URL:</label>
-              <input type="text" id="url" name="url" placeholder="Enter URL" className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" value={url} onChange={handleChange("2")} />
-            </div>
+            <input type="text" id="url" name="url" placeholder="Enter URL" className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" value={url} onChange={handleChange('')} />
+
 
 
             <button id="submitBtn"
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold  px-4 rounded focus:outline-none focus:shadow-outline"
-              onClick={() => send(url)}
+              className=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5  dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+              onClick={() => send(url, weights)}
             >
               Submit
             </button>
@@ -54,53 +85,97 @@ export default function Home() {
 
 
           <div className="overflow-x-auto">
+            <h1 className="font-bold text-xl text-center">Score: {data.analyzeScore}</h1>
             <table className="w-full bg-white border rounded-lg">
               <thead className="bg-gray-100">
                 <tr>
                   <th className="px-4 py-2 text-gray-600 font-bold border">Metric</th>
                   <th className="px-4 py-2 text-gray-600 font-bold border">Value</th>
+                  <th className="px-4 py-2 text-gray-600 font-bold border">Weights</th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
                   <td className="px-4 py-2 border">firstContentfulPaint</td>
-                  <td className="px-4 py-2 border">{data.firstContentfulPaint}</td>
+                  <td className="px-4 py-2 border">{data.measurement.firstContentfulPaint}</td>
+                  <td className="px-4 py-2 border flex align-center">
+                    <label htmlFor="firstContentfulPaint" className="mb-2 text-sm font-medium text-gray-900 ">{weights.firstContentfulPaint} </label>
+                    <input type="range" id="firstContentfulPaint" min={0} max={1} step={0.01} className="w-full h-2 m-2 bg-gray-200 rounded-lg appearance-none cursor-pointer " value={weights.firstContentfulPaint} onChange={handleRangeChange('firstContentfulPaint')} />
+                  </td>
+
+
                 </tr>
                 <tr>
                   <td className="px-4 py-2 border">largestContentfulPaint</td>
-                  <td className="px-4 py-2 border">{data.largestContentfulPaint}</td>
+                  <td className="px-4 py-2 border">{data.measurement.largestContentfulPaint}</td>
+                  <td className="px-4 py-2 border  flex align-center">
+                    <label htmlFor="largestContentfulPaint" className="block mb-2 text-sm font-medium text-gray-900 ">{weights.largestContentfulPaint} </label>
+                    <input type="range" id="largestContentfulPaint" min={0} max={1} step={0.01} className="w-full h-2 m-2 bg-gray-200 rounded-lg appearance-none cursor-pointer " value={weights.largestContentfulPaint} onChange={handleRangeChange('largestContentfulPaint')} />
+                  </td>
                 </tr>
                 <tr>
                   <td className="px-4 py-2 border">firstMeaningfulPaint</td>
-                  <td className="px-4 py-2 border">{data.firstMeaningfulPaint}</td>
+                  <td className="px-4 py-2 border">{data.measurement.firstMeaningfulPaint}</td>
+                  <td className="px-4 py-2 border  flex align-center">
+                    <label htmlFor="firstMeaningfulPaint" className="block mb-2 text-sm font-medium text-gray-900 ">{weights.firstMeaningfulPaint} </label>
+                    <input type="range" id="firstMeaningfulPaint" min={0} max={1} step={0.01} className="w-full h-2 m-2 bg-gray-200 rounded-lg appearance-none cursor-pointer " value={weights.firstMeaningfulPaint} onChange={handleRangeChange('firstMeaningfulPaint')} />
+                  </td>
                 </tr>
                 <tr>
                   <td className="px-4 py-2 border">speedIndex</td>
-                  <td className="px-4 py-2 border">{data.speedIndex}</td>
+                  <td className="px-4 py-2 border">{data.measurement.speedIndex}</td>
+                  <td className="px-4 py-2 border flex align-center">
+                    <label htmlFor="speedIndex" className="block mb-2 text-sm font-medium text-gray-900 ">{weights.speedIndex} </label>
+                    <input type="range" id="speedIndex" min={0} max={1} step={0.01} className="w-full h-2 m-2 bg-gray-200 rounded-lg appearance-none cursor-pointer " value={weights.speedIndex} onChange={handleRangeChange('speedIndex')} />
+                  </td>
                 </tr>
                 <tr>
                   <td className="px-4 py-2 border">totalBlockingTime</td>
-                  <td className="px-4 py-2 border">{data.totalBlockingTime}</td>
+                  <td className="px-4 py-2 border">{data.measurement.totalBlockingTime}</td>
+                  <td className="px-4 py-2 border flex align-center">
+                    <label htmlFor="totalBlockingTime" className="block mb-2 text-sm font-medium text-gray-900 ">{weights.totalBlockingTime} </label>
+                    <input type="range" id="totalBlockingTime" min={0} max={1} step={0.01} className="w-full h-2 m-2 bg-gray-200 rounded-lg appearance-none cursor-pointer " value={weights.totalBlockingTime} onChange={handleRangeChange('totalBlockingTime')} />
+                  </td>
                 </tr>
                 <tr>
                   <td className="px-4 py-2 border">maxPotentialFid</td>
-                  <td className="px-4 py-2 border">{data.maxPotentialFid}</td>
+                  <td className="px-4 py-2 border">{data.measurement.maxPotentialFid}</td>
+                  <td className="px-4 py-2 border flex align-center">
+                    <label htmlFor="maxPotentialFid" className="block mb-2 text-sm font-medium text-gray-900 ">{weights.maxPotentialFid} </label>
+                    <input type="range" id="maxPotentialFid" min={0} max={1} step={0.01} className="w-full h-2 m-2 bg-gray-200 rounded-lg appearance-none cursor-pointer " value={weights.maxPotentialFid} onChange={handleRangeChange('maxPotentialFid')} />
+                  </td>
                 </tr>
                 <tr>
                   <td className="px-4 py-2 border">cumulativeLayoutShift</td>
-                  <td className="px-4 py-2 border">{data.cumulativeLayoutShift}</td>
+                  <td className="px-4 py-2 border">{data.measurement.cumulativeLayoutShift}</td>
+                  <td className="px-4 py-2 border flex align-center">
+                    <label htmlFor="cumulativeLayoutShift" className="block mb-2 text-sm font-medium text-gray-900 ">{weights.cumulativeLayoutShift} </label>
+                    <input type="range" id="cumulativeLayoutShift" min={0} max={1} step={0.01} className="w-full h-2 m-2 bg-gray-200 rounded-lg appearance-none cursor-pointer " value={weights.cumulativeLayoutShift} onChange={handleRangeChange('cumulativeLayoutShift')} />
+                  </td>
                 </tr>
                 <tr>
                   <td className="px-4 py-2 border">serverResponseTime</td>
-                  <td className="px-4 py-2 border">{data.serverResponseTime}</td>
+                  <td className="px-4 py-2 border">{data.measurement.serverResponseTime}</td>
+                  <td className="px-4 py-2 border flex align-center">
+                    <label htmlFor="serverResponseTime" className="block mb-2 text-sm font-medium text-gray-900 ">{weights.serverResponseTime} </label>
+                    <input type="range" id="serverResponseTime" min={0} max={1} step={0.01} className="w-full h-2 m-2 bg-gray-200 rounded-lg appearance-none cursor-pointer " value={weights.serverResponseTime} onChange={handleRangeChange('serverResponseTime')} />
+                  </td>
                 </tr>
                 <tr>
                   <td className="px-4 py-2 border">timeToInteractive</td>
-                  <td className="px-4 py-2 border">{data.timeToInteractive}</td>
+                  <td className="px-4 py-2 border">{data.measurement.timeToInteractive}</td>
+                  <td className="px-4 py-2 border flex align-center">
+                    <label htmlFor="timeToInteractive" className="block mb-2 text-sm font-medium text-gray-900 ">{weights.timeToInteractive} </label>
+                    <input type="range" id="timeToInteractive" min={0} max={1} step={0.01} className="w-full h-2 m-2 bg-gray-200 rounded-lg appearance-none cursor-pointer " value={weights.timeToInteractive} onChange={handleRangeChange('timeToInteractive')} />
+                  </td>
                 </tr>
                 <tr>
                   <td className="px-4 py-2 border">metrics</td>
-                  <td className="px-4 py-2 border">{data.metrics}</td>
+                  <td className="px-4 py-2 border">{data.measurement.metrics}</td>
+                  <td className="px-4 py-2 border flex align-center">
+                    <label htmlFor="metrics" className="block mb-2 text-sm font-medium text-gray-900 ">{weights.metrics} </label>
+                    <input type="range" id="metrics" min={0} max={1} step={0.01} className="w-full h-2 m-2 bg-gray-200 rounded-lg appearance-none cursor-pointer " value={weights.metrics} onChange={handleRangeChange('metrics')} />
+                  </td>
                 </tr>
 
               </tbody>
